98e3cf5fa0f1cf50513479a27051f2797ee5d2a8
Muthulakshmi <muthulakshmi.s@accubits.com>
2022-02-19 11:04:35 +0530

    threed model, update things

--- myapp/src/app/threed-model/threed-model.component.ts
+++ myapp/src/app/threed-model/threed-model.component.ts
@@ -10,8 +10,18 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 })
 export class ThreedModelComponent implements OnInit {
   scene = new THREE.Scene();
-  renderer = new THREE.WebGLRenderer();
-  camera = new THREE.PerspectiveCamera(150, window.innerHeight / window.innerWidth, 0.1, 200)
+  renderer = new THREE.WebGLRenderer({
+
+    alpha: true,
+  
+    antialias: true
+  
+  });
+  camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.1, 80000);
+  //FOV-Field of View
+  //view ratio
+  //near
+  //far
   loader = new GLTFLoader();
   url = "assets/ftm/scene.gltf"
 
@@ -20,11 +30,14 @@ export class ThreedModelComponent implements OnInit {
   ngOnInit() {
     this.renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(this.renderer.domElement);
-    this.camera.position.set(0, 0, 200);
+    this.camera.position.set(0, 5, 0);
     this.camera.lookAt(0, 0, 0);
     this.loader.load(this.url, ((gltf) => {
+      // gltf.scene.scale.set(0, 2, 2);
+      // gltf.scene.position.set(220, 10, 400)
       this.scene.add(gltf.scene);
-      this.camera.position.z = 15
+
+      this.camera.position.z = 7
       this.renderer.render(this.scene, this.camera);
     }), undefined, ((err) => { console.log(err) }))
   }
--- myapp/src/app/update-things/update-things.component.ts
+++ myapp/src/app/update-things/update-things.component.ts
@@ -7,9 +7,27 @@ declare var window
   styleUrls: ['./update-things.component.scss']
 })
 export class UpdateThingsComponent implements OnInit {
+  scene = new THREE.Scene();
+  renderer = new THREE.WebGLRenderer();
+  camera = new THREE.PerspectiveCamera(150, window.innerWidth / window.innerHeight, 0.1, 200);
+  object = new THREE.Object3D({ color: 0x0000ff });
+
   constructor() { }
 
   ngOnInit() {
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(this.renderer.domElement);
+    this.camera.position.set(0, 0, 500);
+    this.camera.lookAt(0, 0, 0);
+   this.scene.add(this.object)
   }
-
+  ngOnDestroy() {
+    let canvasEle : any = document.getElementsByTagName('canvas')
+    console.log(canvasEle)
+    document.activeElement.removeChild(canvasEle[0]);
+    // canvasEle[0].style.display = 'none';
+    this.scene.remove(this.object);
+    this.renderer.render(this.scene, this.camera);
+  
+  }
 }
